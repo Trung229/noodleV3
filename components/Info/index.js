1667ff0fea2ecc66a3557999e955dd50fa093974
Trung <ductrung4263@gmail.com>
2021-12-15 16:44:38 +0700

    handle logic get noodle from firebase

--- components/Info/index.js
+++ components/Info/index.js
@@ -1,14 +1,52 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     Text,
-    View
+    View,
+    Image,
+    ActivityIndicator,
+    BackHandler,
 } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux'
 import moment from 'moment';
-import { fetchAPI } from '../../src/services/noodle'
+import { fetchAPI, updateNoodle } from '../../src/services/noodle'
+import Layout from '../../src/layout'
+import { INFORMATION, NOODLE_AVAILABLE_1, NOODLE_AVAILABLE_2, NOODLE_AVAILABLE_3, NOODLE_IS_TAKEN, BUTTON } from '../../src/utils/constant'
+import styles from './styles'
+import MyButton from './subComponents/myButton'
 
-const Info = ({ route: { params: { id } } }) => {
+
+/**
+ * handle noodles button:
+ * lay mang noodle ma co data isTaken la true
+ * moi khi bam, thi minh se lay phan tu cuoi cung cua mang, va lay ra prop la isTaken va gan no lai la true
+ *
+ * **/
+
+const arrNoodle = [
+    NOODLE_AVAILABLE_1,
+    NOODLE_AVAILABLE_2,
+    NOODLE_AVAILABLE_3
+]
+
+
+
+const Info = ({ route: { params: { id } }, navigation }) => {
     const noodle = useSelector((state) => state?.data?.payload)
+    let count = []
+    const [arrState, setArrState] = useState(noodle?.noodleAvailable)
+    function handleGetNoodle() {
+        let temp_Arr = [...arrState];
+        let temp_Element = { ...temp_Arr[count.length - 1] };
+        temp_Element.isTaken = true;
+        temp_Arr[count.length - 1] = temp_Element
+        setArrState(temp_Arr)
+        if (arrState) {
+            navigation.navigate('Done');
+        }
+    }
+    function goBack() {
+        navigation.navigate('Welcome');
+    }
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch({
@@ -21,18 +59,93 @@ const Info = ({ route: { params: { id } } }) => {
                 type: 'noodle/saveID',
                 payload: 0,
             })
+            dispatch({
+                type: 'noodle/saveData',
+                payload: undefined,
+            })
         }
-    }, [id])
+    }, [])
+    useEffect(() => {
+        setArrState(noodle?.noodleAvailable)
+    }, [noodle])
+    useEffect(() => {
+        dispatch({
+            type: 'noodle/saveNoodle',
+            payload: arrState,
+        })
+        dispatch(updateNoodle)
+    }, [arrState])
+    useEffect(() => {
+        return () => {
+            dispatch({
+                type: 'noodle/saveNoodle',
+                payload: null,
+            })
+        }
+    },[])
     return (
-        <View>
-            {noodle &&
-                <View>
-                    <Text>{noodle.fullName}</Text>
-                    <Text>{noodle.gender}</Text>
-                    <Text>{moment.unix(noodle?.birthDay?.seconds).format('DD MMM YYYY')}</Text>
-                </View>
+        <Layout title={INFORMATION}>
+            {noodle ?
+                <>
+                    <View style={styles.borderOutside}>
+                        <View style={styles.containerInfo}>
+                            <View style={styles.containerLeft}>
+                                <Image
+                                    style={{ width: 100, height: 100, borderRadius: 100 }}
+                                    source={{
+                                        uri: noodle.avatar,
+                                    }}
+                                />
+                            </View>
+                            <View style={styles.containerRight}>
+                                <View style={styles.containerText}>
+                                    <Text style={styles.textBold}>Full Name:</Text>
+                                    <Text style={styles.textNormal}>{noodle.fullName}</Text>
+                                </View>
+                                <View style={styles.containerText}>
+                                    <Text style={styles.textBold}>BirthDay:</Text>
+                                    <Text style={styles.textNormal}>{moment.unix(noodle?.birthDay?.seconds).format('DD MMM YYYY')}</Text>
+                                </View>
+                                <View style={styles.containerText}>
+                                    <Text style={styles.textBold}>Gender:</Text>
+                                    <Text style={styles.textNormal}>{noodle.gender}</Text>
+                                </View>
+                                <View style={styles.containerText}>
+                                    <Text style={styles.textBold}>Department:</Text>
+                                    <Text style={styles.textNormal}>{noodle.department}</Text>
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={styles.containerNoodle}>
+                        {noodle.noodleAvailable.map((item, index) => {
+                            const randomNoodle = Math.floor(Math.random() * arrNoodle.length);
+                            if (!item.isTaken) count.push(item)
+                            return (!item.isTaken ?
+                                <Image
+                                    resizeMode="contain"
+                                    style={{ width: 100, height: 100, marginTop: 10 }}
+                                    key={index}
+                                    source={arrNoodle[randomNoodle]} /> :
+                                <Image
+                                    resizeMode="contain"
+                                    key={index}
+                                    style={{ width: 100, height: 100, marginTop: 10 }}
+                                    source={NOODLE_IS_TAKEN} />
+                            )
+                        })}
+                    </View>
+                    <View style={styles.containerTextAvailable}>
+                        <Text style={styles.textLength}>{count.length}</Text>
+                        <Text style={styles.text}>cups of noodles left this month</Text>
+                    </View>
+                    {count.length <= 0 ?
+                        <MyButton title="Come back next month" handleEvent={goBack} /> :
+                        <MyButton title="Get your noodles" handleEvent={handleGetNoodle} />}
+                </> :
+                <ActivityIndicator size="large" color="#00ff00" />
             }
-        </View>
+        </Layout>
     )
 }
 export default Info;
